51b3a38ab570f4223c0ec91ea7a310e689ff45a4
Benton Green <bentongreen076@gmail.com>
2015-08-24 12:58:32 -0700

    firebase and local storage changes commit

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,22 @@
 'use strict'
 $(document).ready(function(){
 
+//set valid logins
+localStorage.setItem("username1", "student");
+localStorage.setItem("username2", "faculty");
+localStorage.setItem("username3", "benton");
+localStorage.setItem("username4", "bella");
+localStorage.setItem("username5", "kelsey");
+localStorage.setItem("username6", "ryan");
+localStorage.setItem("password", "codefellows");
+
 	$("#loginButton").click(function(){
 		var username = $('#usernameId').val();
 		var password = $('#passwordId').val();
 		console.log(username);
 		console.log(password);
 
-		if(password === "codefellows"){
+		if(password === localStorage.getItem("password"){
 			localStorage.setItem("login", "true");
 			window.location.href = "index.html";
 		}
